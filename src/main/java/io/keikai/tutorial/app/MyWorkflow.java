1736f8ae69349741990fa9975c7f50e4cf436a1f
hawkchen <hawkchen@potix.com>
2018-10-08 15:42:48 +0800

    correct the checking criteria in submissionSelectionListener 

--- src/main/java/io/keikai/tutorial/app/MyWorkflow.java
+++ src/main/java/io/keikai/tutorial/app/MyWorkflow.java
@@ -258,7 +258,7 @@ public class MyWorkflow {
             @Override
             public void onEvent(RangeEvent rangeEvent) throws Exception {
                 if (!rangeEvent.getWorksheet().getName().equals(SHEET_SUBMISSION)
-                    && (rangeEvent.getColumn() <2 || rangeEvent.getColumn() >6)) { //inside table columns
+                    || (rangeEvent.getColumn() <2 || rangeEvent.getColumn() >6)) { //inside table columns
                     return;
                 }
                 Range idCell = spreadsheet.getRange(rangeEvent.getRange().getRow(), STARTING_COLUMN);
